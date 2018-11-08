8516017adfc82f3493cb77d1a60083783f6dd5d7
reventhao <ren895873@gmail.com>
2018-11-08 15:04:17 +0800

    deploy

---
+++ precache-manifest.cf523617c9f3f8be422ec8caeb9c5e97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "1fcc9a0d563068f5f3cb9e05830ee312",
+    "url": "/img/bg4.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d5fde73b5accac2b7a18",
+    "url": "/js/app.72b20b7c.js"
+  },
+  {
+    "revision": "586e69fe8b8e3f17de1aacf3ec5bb7a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6557c71c13936ac46a1a",
+    "url": "/js/chunk-vendors.7b97e4b9.js"
+  },
+  {
+    "revision": "6905c6ed7eb8da5356b9",
+    "url": "/js/about.2083e280.js"
+  },
+  {
+    "revision": "c2d6ddcfd23d630e25c28a4081ba2c4b",
+    "url": "/img/bg3.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/img/bg2.jpg"
+  },
+  {
+    "revision": "0bf01ef3b95f5d14ec5c6a75ed51a3ff",
+    "url": "/img/bg1.jpg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "6557c71c13936ac46a1a",
+    "url": "/css/chunk-vendors.ea3fa8e3.css"
+  },
+  {
+    "revision": "d5fde73b5accac2b7a18",
+    "url": "/css/app.8f251194.css"
+  },
+  {
+    "revision": "6905c6ed7eb8da5356b9",
+    "url": "/css/about.1eeed41a.css"
+  }
+];
